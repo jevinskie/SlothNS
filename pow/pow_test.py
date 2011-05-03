523bbef358c60d96b31028490d41a0b475385cd2
Jevin Sweval <jevinsweval@gmail.com>
2011-05-03 01:19:13 -0400

    added memory usage information

--- pow/pow_test.py
+++ pow/pow_test.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 import sys
+import resource
 from pow import *
+
+n = int(sys.argv[1])
+l = int(sys.argv[2])
 
 print "creating pow"
 sys.stdout.flush()
-p = pow(24, 0)
+p = pow(n, 0)
 
 print "creating req"
 sys.stdout.flush()
-req = p.create_req(20)
+req = p.create_req(l)
 
 print "creating res"
 sys.stdout.flush()
@@ -20,3 +24,10 @@ sys.stdout.flush()
 print req.verify_res(res)
 sys.stdout.flush()
 
+print "memory usage:"
+u = resource.getrusage(resource.RUSAGE_SELF)
+mb = u.ru_maxrss / (1.0 * 2**10)
+print "TOTAL: %d MB" % mb
+
+if len(sys.argv) > 3:
+    raw_input("paused... press enter to continue\n")
