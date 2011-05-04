f1cd1e5c903cb0b52725042908da78c5eca25264
Jevin Sweval <jevinsweval@gmail.com>
2011-05-04 14:16:36 -0400

    Changed slothresolv from a script to a module with a query() function.
    HACK HACK HACK:
    query() can only be called once per thread!!! It stops the Twisted
    reactor and reactors cannot be restarted. This is fine because the
    DDoS simulation performs one SlothNS lookup per thread lifetime.

--- slothresolv.py
+++ slothresolv.py
@@ -9,6 +9,8 @@ from twisted.internet import reactor, defer
 import operator
 from pow import *
 
+result = None
+
 class SlothNSResolver(client.Resolver):
 
     def _lookup_queries(self, queries, timeout):
@@ -30,22 +32,22 @@ class SlothNSResolver(client.Resolver):
 
     @defer.inlineCallbacks
     def lookupAddress(self, name, timeout = None):
-        res = yield client.Resolver.lookupAddress(self, name, timeout)
+        global result
+        res = yield client.Resolver.lookupIPV6Address(self, name, timeout)
         challenge = res[2][0]
         req = pow_req(wire = challenge.payload.payload)
         res = req.create_res()
         query_aaaa = dns.Query(name = name, type = dns.AAAA)
         query_res = dns.Query(name = res.pack(), type = dns.NULL)
         res = yield self._lookup_queries([query_aaaa, query_res], timeout)
-        defer.returnValue(res)
+        result = res
+        reactor.stop()
 
 resolver = SlothNSResolver(servers=[('127.0.0.1', 5454)])
 
-r = resolver.lookupAddress('google.com')
-def print_ip(result):
-    print socket.inet_ntop(socket.AF_INET6, result[0][0].payload.address)
-    reactor.stop()
+def query():
+    global result
+    reactor.callWhenRunning(resolver.lookupAddress, 'google.com')
+    reactor.run()
+    return result
 
-r.addCallback(print_ip)
-
-reactor.run()
