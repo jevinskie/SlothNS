f65e5986b0bc511e76732674f275f27d15b3da83
Jevin Sweval <jevinsweval@gmail.com>
2011-05-04 19:53:28 -0400

    do a PoW query in the stressor

--- stressor.py
+++ stressor.py
@@ -9,6 +9,8 @@ from Queue import PriorityQueue, Empty
 import os
 from slothresolv import query
 from multiprocessing import Pool, Manager
+sys.path.append('./pow')
+from pow import *
 
 url = 'http://172.18.48.222:8080/fib.php?n='
 
@@ -20,6 +22,8 @@ class fetcher(Thread):
         self.work_q = work_q
 
 def fetch(attacker, id, res_q, wait_q_q):
+    global shared_pow
+    pow_obj = shared_pow
     if attacker:
         n = 100
     elif random.uniform(0, 1) > 0.9:
@@ -30,23 +34,20 @@ def fetch(attacker, id, res_q, wait_q_q):
         delay = 0
     else:
         delay = random.expovariate(1.0/45)
-    print "starting fetch"
     start = time.time()
-    print "starting query"
-    query('google.com', id = id)
-    print "query is done"
+    query('google.com', id = id, pow_obj = pow_obj)
     try:
-        print "starting url open"
         urllib2.urlopen(url + str(n) + "&id=%d" % id)
-        print "url open done"
         error = 0
     except urllib2.URLError:
-        print "url open failed"
         error = 1
     end = time.time()
     res_q.put((n, end - start, error))
-    print "putting in wait_q_q"
     wait_q_q.put((time.time() + delay, attacker, id))
+
+p = pow(22, 0)
+shared_pow = pow(p.n, p.seed, share = p)
+
 
 def main(argv=None):
     if argv is None:
@@ -66,11 +67,11 @@ def main(argv=None):
     print "Running the gauntlet:"
     id = id_base
     num_clients = 0
-    for i in range(10):
+    for i in range(2):
         wait_q.put((time.time(), False, id))
         id += 1
         num_clients += 1
-    for i in range(20):
+    for i in range(100):
         wait_q.put((time.time(), True, id))
         id += 1
         num_clients += 1
@@ -79,38 +80,27 @@ def main(argv=None):
 
     while True:
         t, attacker, id = wait_q.get()
-        print "got a wait_q"
         delta = t - time.time()
         delay = max(delta, 0)
         if delay > 0:
-            print "delaying"
             Timer(delay, pool.apply_async, (fetch, (attacker, id, res_q, wait_q_q))).start()
         else:
-            print "no delay"
             pool.apply_async(fetch, (attacker, id, res_q, wait_q_q))
         wait_q.task_done()
 
-        print "empty wait_q_q"
         while not wait_q_q.empty():
-            print "got a wait_q_q"
             wait_q.put(wait_q_q.get())
 
         # we have to make sure there is at least one element in wait_q before the end of the loop
         if wait_q.empty():
-            print "wait_q is empty"
             r = wait_q_q.get()
-            print "got a wait_q_q"
             wait_q.put(r)
 
-        print "empty res_q"
         while not res_q.empty():
             res = res_q.get()
-            print "got a res_q"
             out_file.write("%d, %f, %d\n" % (res[0], res[1], res[2]))
             out_file.flush()
-        print "done with empty queue"
 
-    print "done with infinite loop"
     while not res_q.empty():
         res = res_q.get()
         out_file.write("%d, %f, %d\n" % (res[0], res[1], res[2]))
